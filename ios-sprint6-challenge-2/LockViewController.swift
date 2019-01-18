107fdf38d94805249efa625243bc29a60b66889d
David Doswell <davidoliverdoswell@icloud.com>
2019-01-18 08:26:23 -0800

    Initial Commit

---
+++ ios-sprint6-challenge-2/LockViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LockViewController.swift
+//  ios-sprint6-challenge-2
+//
+//  Created by David Doswell on 1/18/19.
+//  Copyright © 2019 David Doswell. All rights reserved.
+//
+
+import UIKit
+
+class LockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ ios-sprint6-challenge-2/UnlockViewController.swift
@@ -0,0 +1,25 @@
+//
+//  UnlockViewController.swift
+//  ios-sprint6-challenge-2
+//
+//  Created by David Doswell on 1/18/19.
+//  Copyright © 2019 David Doswell. All rights reserved.
+//
+
+import UIKit
+
+class UnlockViewController: UIViewController {
+    
+    @IBOutlet weak var reset: UIBarButtonItem!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func resetLock(_ sender: Any) {
+    
+    }
+    
+}
